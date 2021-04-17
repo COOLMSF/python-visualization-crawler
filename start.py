4d39b32f0e8e3e09c3d4954e3d40a56cfb601fba
coolmsf <coolder.hsl@outlook.com>
2021-04-17 10:43:56 +0800

    this is the start script

--- start.py
+++ start.py
@@ -1,20 +1,25 @@
 import os
 import multiprocessing
 
+# Crawler function
 def crawler(topic):
     cmd = "python WeiboTopicScrapy.py %s" % topic
     os.system(cmd)
 
+# Get all hot topics, and let threads to crawl them.
 print("Get top topics")
 os.system("python WeiboTopic.py > topic/top_topics.txt")
 f = open("topic/top_topics.txt", "r")
 lines = f.readlines()
 top_topics = []
 for line in lines:
+    # Filter data
     c = line.split(" ")[0]
+    # We don't need "0\n"
     if c != "0\n":
         top_topics.append(line.split(" ")[0])
 
+# Make threads\workers
 for top_topic in top_topics:
     print("crawler topic %s" % top_topic)
     p = multiprocessing.Process(target=crawler, args=(top_topic, ))
